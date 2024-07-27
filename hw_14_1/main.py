13b3a4cd1811d3322b69fe8085008ad47e98be41
Alex <atikakovr@mail.ru>
2024-07-27 15:17:42 +0300

    ДЗ 14-1

---
+++ hw_14_1/main.py
@@ -0,0 +1,78 @@
+# 2024/01/23 00:00|Создание, Изменение и Удаление элементов
+
+import sqlite3
+
+DATABASE = 'my_bd.db'
+
+
+def init_data_base(database):
+    # Инициализация бд
+    with sqlite3.connect(database) as con_db:
+        cur = con_db.cursor()
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER,
+        balance INTEGER NOT NULL
+        )''')
+        con_db.commit()
+
+
+def add_elem_db(cur, username, email, age, balance):
+    cur.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)",
+                (username, email, age, balance))
+
+
+def add_data():
+    with sqlite3.connect(DATABASE) as con_db:
+        cur = con_db.cursor()
+        for i in range(10):
+            add_elem_db(cur, f'username{i + 1}', f'email{i + 1}@email.ru',
+                        (i + 1) * 10, 1000)
+        con_db.commit()
+
+
+def update_balance_db(cur, balance, username):
+    cur.execute("UPDATE Users SET balance = ? WHERE username = ?",
+                    (balance, username))
+
+
+def change_balance(new_balance):
+    with sqlite3.connect(DATABASE) as con_db:
+        cur = con_db.cursor()
+        cur.execute("SELECT id, username FROM Users WHERE id % 2 <> 0")
+        for user in cur.fetchall():
+            update_balance_db(cur, new_balance, username=user[1])
+        con_db.commit()
+
+
+def del_elem_db(cur, username):
+    cur.execute("DELETE FROM Users WHERE username = ?", (username, ))
+
+
+def remove_users():
+    with sqlite3.connect(DATABASE) as con_db:
+        cur = con_db.cursor()
+        cur.execute("SELECT id, username FROM Users WHERE (id % 3 = 1) OR (id = 1)")
+        for user in cur.fetchall():
+            del_elem_db(cur, username=user[1])
+        con_db.commit()
+
+
+def get_age(age):
+    with sqlite3.connect(DATABASE) as con_db:
+        cur = con_db.cursor()
+        cur.execute(f"SELECT age, username, email, age, balance FROM Users WHERE age <> {age}")
+        for user in cur.fetchall():
+            print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
+        con_db.commit()
+
+
+if __name__ == '__main__':
+    init_data_base(DATABASE)
+    add_data()
+    change_balance(500)
+    remove_users()
+    get_age(age=60)
