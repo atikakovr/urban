5f7501cb8f2accb27724946d43a273bb06438c0e
Alex <atikakovr@mail.ru>
2024-08-26 23:48:22 +0300

    Задание 16 2

--- hw_16_1/main.py
+++ hw_16_1/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -18,7 +19,14 @@ async def users_id(user_id):
 	return f"Вы вошли как пользователь № {user_id}"
 
 
+@app.get("/user/{username}/{user_id}")
+async def news(
+		username:
+		Annotated[str, Path(min_length=3, max_length=15, description="Введите имя пользователя", example="Alex")],
+		user_id: int = Path(le=1500, ge=0)):
+	return {"message": f"Hello, {username}:{user_id}"}
+
+
 @app.get("/user")
 async def user_info(username: str = 'Alex', age: int = 54):
 	return {"User": username, "age": age}
-
